01209675788680d8a0172ddcb554a09ba5d43d30
JosePereiraF <jose222125santos@gmail.com>
2024-05-01 13:04:38 -0300

    add id botoes produtos

---
+++ TrabalhoFinalFrontEnd/scripts/produtos.js
@@ -0,0 +1,14 @@
+const btnInfoCel1 =  document.querySelector("#btnCel1");
+btnInfoCel1.addEventListener("click", infoCelular);
+
+function infoCelular(){
+
+   switch (key) {
+    case value:
+        
+        break;
+   
+    default:
+        break;
+   }
+}
