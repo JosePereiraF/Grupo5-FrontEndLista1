056745cbdc18ad30a32de9f7819b271ee7dac547
JosePereiraF <jose222125santos@gmail.com>
2024-05-03 18:03:37 -0300

    ajustes finais

---
+++ TrabalhoFinalFrontEnd/scripts/checkout.js
@@ -0,0 +1,8 @@
+const finalizar = document.querySelector("#botao-confirmar-compra")
+finalizar.addEventListener("click",()=>{
+    finalizarCadastro();
+}
+)
+function finalizarCadastro(){
+    alert("Compra finalizada com sucesso!")
+}
